8bdd895dfab1e2ff59405f2c38497cab77330b70
eon <72848713+Eonexe@users.noreply.github.com>
2024-06-01 21:17:24 +1000

    changes out of default template

--- src/main/java/dev/eonexe/additions/Additions.java
+++ src/main/java/dev/eonexe/additions/Additions.java
@@ -1,8 +1,8 @@
-package com.example.addon;
+package dev.eonexe.additions;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import dev.eonexe.additions.commands.CommandExample;
+import dev.eonexe.additions.hud.HudExample;
+import dev.eonexe.additions.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -13,23 +13,26 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class Additions extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("Additions");
+    public static final HudGroup HUD_GROUP = new HudGroup("Additions");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Eonexe's Meteor Additions");
+
+        Modules register = Modules.get();
+        Hud hud = Hud.get();
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        register.add(new ModuleExample());
 
         // Commands
         Commands.add(new CommandExample());
 
         // HUD
-        Hud.get().register(HudExample.INFO);
+        hud.register(HudExample.INFO);
     }
 
     @Override
@@ -39,11 +42,11 @@ public class AddonTemplate extends MeteorAddon {
 
     @Override
     public String getPackage() {
-        return "com.example.addon";
+        return "dev.eonexe.additions";
     }
 
     @Override
     public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+        return new GithubRepo("Eonexe", "additions");
     }
 }
